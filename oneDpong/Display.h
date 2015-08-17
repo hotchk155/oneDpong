43352e919858cb9d6637d086dc475fd12ef309a9
Jason Hotchkiss <jason_hotchkiss@hotmail.com>
2015-08-17 23:21:20 +0100

    charset

--- oneDpong/Display.h
+++ oneDpong/Display.h
@@ -4,7 +4,6 @@ class CDisplay
     void showDigits(int a, int b, byte shift);
     void showCount(int i);
     void showScores(int a, int b);
-    
-    
+    void showText(const char *s, int ofs);     
 };
 extern CDisplay Display;
--- oneDpong/GameMenu.h
+++ oneDpong/GameMenu.h
@@ -28,18 +28,32 @@ Strip Length
 */
 class CGameMenu
 {
+  unsigned long m_nextEventTime;
+  int m_scroll;
 public:  
+  CGameMenu() {
+    m_nextEventTime = 0;
+    m_scroll = 0;
+  }
   void render(APA102& strip) {
-    static byte q = 0;
-    byte qq = 255;
-      for(int i=0; i<strip.m_numLeds; ++i) {
-        strip.m_data[i] = RGB_LED(qq,qq,qq);
-
-      }
+//    static byte q = 0;
+//    byte qq = 255;
+//      for(int i=0; i<strip.m_numLeds; ++i) {
+//        strip.m_data[i] = RGB_LED(qq,qq,qq);
+//
+//      }
   }
   
-  byte  run(APA102& strip, unsigned long ms) {
-    return 0;
+  byte  run(APA102& strip, unsigned long ms) {    
+    if( ms > m_nextEventTime) {
+      Display.showText("Bollocks!! ` ",m_scroll);
+//      Display.showText("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefgjhijklmnopqrstuvwxyz0123456789( ! ? )", m_scroll);      
+      if(++m_scroll > 60)
+        m_scroll = -15;
+      m_nextEventTime = ms + 100;      
+    }
+      
+    return 1;
   }  
 };
 
